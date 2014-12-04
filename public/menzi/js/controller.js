7233891c647e59fc510c566a266251d71220cfa3
yubaoquan <1079139460@qq.com>
2014-12-05 00:16:15 +0800

    Angular
    第一步,顺利成功,用AngularJS的控制器插入了一个图片资源;

---
+++ public/menzi/js/controller.js
@@ -0,0 +1,8 @@
+var menziApp = angular.module('menziApp', []);
+
+menziApp.controller('menziController', function ($scope) {
+  $scope.gay = 
+    {"imageUrl": "../images/bili.gif"}
+  ;
+  console.log('here2333');
+});
